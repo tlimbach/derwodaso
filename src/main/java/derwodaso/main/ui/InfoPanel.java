befe55db7100f990c12536291f1dd73d1b7388ca
Thorsten Limbach <thorsten.limbach@ymail.com>
2019-05-20 21:28:26 +0200

    Layout angepasst

--- src/main/java/derwodaso/main/ui/InfoPanel.java
+++ src/main/java/derwodaso/main/ui/InfoPanel.java
@@ -19,31 +19,33 @@ import java.awt.Dimension;
  * @author tlimbach
  */
 class InfoPanel extends JPanel {
-
+    
     JLabel lblImage;
-
+    
     public InfoPanel() {
         init();
     }
-
+    
     private void init() {
         lblImage = new JLabel();
-
+        
         lblImage.setPreferredSize(new Dimension(300, 300));
         lblImage.setHorizontalAlignment(JLabel.CENTER);
-
+        
         setLayout(new BorderLayout());
         add(lblImage, BorderLayout.CENTER);
     }
-
+    
     public void setMoviePoster(URL url) {
-
+        
         if (url != null) {
             ImageIcon image = new ImageIcon(url);
             image = new ImageIcon(
                     Helper.getScaledImage(image.getImage(), (370 * image.getIconWidth()) / image.getIconHeight(), 370));
             lblImage.setIcon(image);
+        } else {
+            lblImage.setIcon(null);
         }
-
+        
     }
 }
